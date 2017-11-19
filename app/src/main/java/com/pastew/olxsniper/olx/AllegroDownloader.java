5d4822471021d712758b33eca6490b3642ab9dee
Pastew <kpastew@gmail.com>
2017-11-19 22:20:51 +0100

    Allegro Downloader without city and price

---
+++ app/src/main/java/com/pastew/olxsniper/olx/AllegroDownloader.java
@@ -0,0 +1,75 @@
+package com.pastew.olxsniper.olx;
+
+import android.util.Log;
+
+import com.pastew.olxsniper.MainActivity;
+import com.pastew.olxsniper.Utils;
+import com.pastew.olxsniper.db.Offer;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class AllegroDownloader extends WebDownloader {
+    @Override
+    public List<Offer> downloadOffersFromWeb(String url) {
+        if (!canHandleLink(url)) {
+            throw new InputMismatchException();
+        }
+
+        List<Offer> result = new ArrayList<>();
+
+        Document doc;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            Log.e(MainActivity.TAG, "IOException, maybe SocketTimeoutException");
+            e.printStackTrace();
+            return result;
+        }
+
+        Elements elements = doc.getElementsByClass("fa72b28");
+
+        if (elements == null) {
+            Log.e(MainActivity.TAG, "elemens is null. ");
+            return result;
+        }
+
+        for (Element offerElement : elements) {
+            if (offerElement == null) {
+                Log.e(MainActivity.TAG, "offerElement is null. ");
+                continue;
+            }
+            String title = offerElement.getElementsByTag("a").first().html();
+            String link = offerElement.getElementsByTag("a").first().attr("href");
+
+            //Element e = offerElement.getElementsByClass("e82f23a").first();
+            //String priceString = e.html();
+            String priceString = "unknown";
+
+
+            String city = "unknown";
+
+            Offer o = new Offer(title, Utils.parsePrice(priceString), link, city);
+
+            if (o.promoted && IGNORE_PROMOTED_OFFERS) {
+                Log.d(MainActivity.TAG, String.format("Ignored promoted offer: %s", o.link));
+            } else {
+                result.add(o);
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    boolean canHandleLink(String url) {
+        return url.contains("allegro.pl");
+    }
+}
--- app/src/test/java/com/pastew/olxsniper/DownloadersTest.java
+++ app/src/test/java/com/pastew/olxsniper/DownloadersTest.java
@@ -1,6 +1,7 @@
 package com.pastew.olxsniper;
 
 import com.pastew.olxsniper.db.Offer;
+import com.pastew.olxsniper.olx.AllegroDownloader;
 import com.pastew.olxsniper.olx.GumtreeDownloader;
 import com.pastew.olxsniper.olx.OfferDownloaderManager;
 import com.pastew.olxsniper.olx.OlxDownloader;
@@ -11,30 +12,42 @@ import java.util.List;
 
 public class DownloadersTest {
 
-    static final String URL = "https://www.olx.pl/elektronika/komputery/akcesoria-i-czesci/q-gtx/";
-    static final String URL_PRACA = "https://www.olx.pl/praca/krakow/";
-    static final String URL_AUDI = "https://www.olx.pl/motoryzacja/samochody/audi/a3/krakow/?search%5Bfilter_float_enginesize%3Afrom%5D=1500&search%5Bfilter_float_milage%3Ato%5D=200000";
-    static final String URL_EXCHANGE = "https://www.olx.pl/zamienie/";
-    static final String URL_TMP = "https://www.olx.pl/elektronika/telefony-komorkowe/q-iphone";
-
-    static final String URL_GUMTREE_IPHONE = "https://www.gumtree.pl/s-elektronika/iphone/v1c9237q0p1";
     @Test
     public void olxDownloaderTest() {
+        final String URL = "https://www.olx.pl/elektronika/komputery/akcesoria-i-czesci/q-gtx/";
+        final String URL_PRACA = "https://www.olx.pl/praca/krakow/";
+        final String URL_AUDI = "https://www.olx.pl/motoryzacja/samochody/audi/a3/krakow/?search%5Bfilter_float_enginesize%3Afrom%5D=1500&search%5Bfilter_float_milage%3Ato%5D=200000";
+        final String URL_EXCHANGE = "https://www.olx.pl/zamienie/";
+        String URL_OLX = "https://www.olx.pl/elektronika/telefony-komorkowe/q-iphone";
 
         OlxDownloader olxDownloader = new OlxDownloader();
-        List<Offer> offerList = olxDownloader.downloadOffersFromWeb(URL_TMP);
+        List<Offer> offerList = olxDownloader.downloadOffersFromWeb(URL_OLX);
 
-        for(Offer o : offerList){
+        for (Offer o : offerList) {
             System.out.println(String.format("%s, %s, %s, %s", o.title, o.price, o.link, o.city));
         }
     }
 
     @Test
     public void gumtreeDownloaderTest() {
+        String URL_GUMTREE_IPHONE = "https://www.gumtree.pl/s-elektronika/iphone/v1c9237q0p1";
+
         GumtreeDownloader gumtreeDownloader = new GumtreeDownloader();
         List<Offer> offerList = gumtreeDownloader.downloadOffersFromWeb(URL_GUMTREE_IPHONE);
 
-        for(Offer o : offerList){
+        for (Offer o : offerList) {
+            System.out.println(String.format("%s, %s, %s, %s", o.title, o.price, o.link, o.city));
+        }
+    }
+
+    @Test
+    public void allegroDownloaderTest() {
+        String URL_ALLEGRO_GTX = "https://allegro.pl/kategoria/obraz-i-grafika-karty-graficzne-257236?string=gtx%201060&order=n&stan=używane";
+
+        AllegroDownloader allegroDownloader = new AllegroDownloader();
+        List<Offer> offerList = allegroDownloader.downloadOffersFromWeb(URL_ALLEGRO_GTX);
+
+        for (Offer o : offerList) {
             System.out.println(String.format("%s, %s, %s, %s", o.title, o.price, o.link, o.city));
         }
     }
