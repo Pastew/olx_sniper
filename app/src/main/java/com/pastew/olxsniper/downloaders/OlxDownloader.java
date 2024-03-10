80363d02a67f37647eecd0fe81e70e0ed8a3ac72
Pastew <kpastew@gmail.com>
2024-03-11 00:14:36 +0100

    fix: add htmlunity, it somehow works with olx

--- app/src/main/java/com/pastew/olxsniper/downloaders/OlxDownloader.java
+++ app/src/main/java/com/pastew/olxsniper/downloaders/OlxDownloader.java
@@ -2,12 +2,17 @@ package com.pastew.olxsniper.downloaders;
 
 import android.util.Log;
 
-import com.pastew.olxsniper.db.Offer;
-import com.pastew.olxsniper.Utils;
+import org.htmlunit.WebClient;
+import org.htmlunit.html.DomNode;
+import org.htmlunit.html.HtmlPage;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.pastew.olxsniper.db.Offer;
+import com.pastew.olxsniper.Utils;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,49 +29,64 @@ public class OlxDownloader extends AbstractDownloader {
 
         List<Offer> result = new ArrayList<>();
 
-        Document doc;
-        try {
-            doc = WebDownloader.downloadDocumentJsoup(url);
+        try (WebClient webClient = new WebClient()) {
+
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            // Enable JavaScript support
+            //webClient.getOptions().setJavaScriptEnabled(true);
+
+            // Download the page using HtmlUnit
+            HtmlPage page = webClient.getPage(url);
+
+            // Wait for asynchronous content to load
+            webClient.waitForBackgroundJavaScript(5000);
+
+            // Parse the page content with JSoup
+            Document doc = Jsoup.parse(page.asXml());
+
+            // Continue with your existing JSoup-based code
+            Elements elements = doc.getElementsByClass("css-1apmciz");
+
+            if (elements == null) {
+                Log.e(TAG, "elements is null. ");
+                return result;
+            }
+
+            for (Element offerElement : elements) {
+                if (offerElement == null) {
+                    Log.e(TAG, "offerElement is null. ");
+                    continue;
+                }
+
+                Element priceElement = offerElement.getElementsByAttributeValue("data-testid", "ad-price").first();
+                if (priceElement == null) {
+                    Log.e(TAG, "priceElement is null. ");
+                    continue;
+                }
+
+                String priceString = priceElement.text();
+
+                String title = h3.getElementsByTag("strong").first().html();
+
+                String link = h3.getElementsByTag("a").first().attr("href");
+                String city = offerElement.getElementsByTag("tr").get(1).getElementsByTag("p").get(0).getElementsByTag("span").first().text();
+
+                Offer o = new Offer(title, Utils.parsePrice(priceString), link, city);
+
+                if (o.promoted && IGNORE_PROMOTED_OFFERS) {
+                    Log.d(TAG, String.format("Ignored promoted offer: %s", o.link));
+                } else {
+                    result.add(o);
+                }
+            }
+
         } catch (IOException e) {
             Log.e(TAG, "IOException, maybe SocketTimeoutException");
             e.printStackTrace();
-            return result;
         }
-
-        Elements elements = doc.getElementsByClass("offer");
-
-        if (elements == null) {
-            Log.e(TAG, "elemens is null. ");
-            return result;
-        }
-
-        for (Element offerElement : elements) {
-            if (offerElement == null) {
-                Log.e(TAG, "offerElement is null. ");
-                continue;
-            }
-
-            Element priceElement = offerElement.getElementsByClass("price").first();
-            if (priceElement == null) {
-                Log.e(TAG, "priceElement is null. ");
-                continue;
-            }
-
-            String priceString = priceElement.getElementsByTag("strong").first().html();
-
-            Element h3 = offerElement.getElementsByTag("h3").first();
-            String title = h3.getElementsByTag("strong").first().html();
-
-            String link = h3.getElementsByTag("a").first().attr("href");
-            String city = offerElement.getElementsByTag("tr").get(1).getElementsByTag("p").get(0).getElementsByTag("span").first().text();
-
-            Offer o = new Offer(title, Utils.parsePrice(priceString), link, city);
-
-            if (o.promoted && IGNORE_PROMOTED_OFFERS) {
-                Log.d(TAG, String.format("Ignored promoted offer: %s", o.link));
-            } else {
-                result.add(o);
-            }
+        catch (Exception e) {
+            Log.e(TAG, "Exception occured" + e.getMessage());
+            e.printStackTrace();
         }
 
         return result;
